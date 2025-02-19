f63b8433dae6b0e38b464afe06eaa3e8801a01d6
candymask0712 <candymask0712@gmail.com>
2025-02-19 13:56:24 +0900

    chore: 주석 제거

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
 
@@ -15,10 +14,10 @@ const eslintConfig = [
   }),
   {
     ignores: [
-      '.next', // Next.js 빌드 파일
-      'node_modules', // 설치된 패키지
-      'dist', // 번들된 빌드 폴더
-      'coverage', // 테스트 커버리지 폴더
+      '.next',
+      'node_modules',
+      'dist',
+      'coverage',
     ],
   },
 ]
